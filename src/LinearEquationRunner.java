6a7d4e60e366accc5b102fc45868d9a65c5a63da
Jonathan <jdonalds100@gmail.com>
2023-11-10 23:59:43 -0500

    11:59

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,21 +1,6 @@
 public class LinearEquationRunner {
     public static void main(String[] args) {
-        /*LinearEquationLogic unit2Project = new LinearEquationLogic();
-        unit2Project.start();*/
-        int x1 = 16;
-        int y1 = -2;
-        int x2 = 3;
-        int y2 = -2;
-        LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
-        System.out.println("Equation: " + equation.equation());
-        System.out.println("Slope: " + equation.slope());
-        System.out.println("y-intercept: " + equation.yIntercept());
-        System.out.println("Distance: " + equation.distance());
-        System.out.println();
-        System.out.println("----- Line info -----");
-        System.out.println(equation.lineInfo());
-        System.out.println();
-        double testX = 4;
-        System.out.println("Coordinate for x: " + equation.coordinateForX(testX));
+        LinearEquationLogic unit2Project = new LinearEquationLogic();
+        unit2Project.start();
     }
 }
